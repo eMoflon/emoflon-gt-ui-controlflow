47a6865a8e61fae89e497c913835d8445a4762b9
Sascha Edwin Zander <sascha@sezander.de>
2018-02-08 18:17:45 +0100

    started documentation

--- org.moflon.gt.mosl.controlflow.language.ui/src/org/moflon/gt/mosl/controlflow/language/ui/highlighting/rules/PatternNameItalicHighlightingRule.java
+++ org.moflon.gt.mosl.controlflow.language.ui/src/org/moflon/gt/mosl/controlflow/language/ui/highlighting/rules/PatternNameItalicHighlightingRule.java
@@ -5,11 +5,9 @@ import org.eclipse.swt.SWT;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.ui.editor.utils.TextStyle;
 import org.moflon.gt.mosl.controlflow.language.moslControlFlow.PatternReference;
-import org.moflon.gt.mosl.controlflow.language.moslControlFlow.PatternStatement;
 import org.moflon.gt.mosl.ide.ui.highlighting.AbstractHighlightProviderController;
 import org.moflon.gt.mosl.ide.ui.highlighting.RegisterRule;
 import org.moflon.gt.mosl.ide.ui.highlighting.rules.AbstractHighlightingRule;
-import org.moflon.gt.mosl.pattern.language.moslPattern.PatternDef;
 
 @RegisterRule
 public class PatternNameItalicHighlightingRule extends AbstractHighlightingRule {
